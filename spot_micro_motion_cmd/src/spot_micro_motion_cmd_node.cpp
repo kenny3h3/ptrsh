0e8bb0ae663e7fb23c9d23f9567d01fc5be25b9d
kenny3h3 <reinhard@nuculeus.at>
2025-09-07 19:03:38 +0200

    AS

--- spot_micro_motion_cmd/src/spot_micro_motion_cmd_node.cpp
+++ spot_micro_motion_cmd/src/spot_micro_motion_cmd_node.cpp
@@ -1,43 +1,26 @@
-// Node file to create object and initialising the ROS node
-#include "spot_micro_motion_cmd.h" 
-#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include "spot_micro_motion_cmd/spot_micro_motion_cmd.h"
 
+using namespace std::chrono_literals;
 
-int main(int argc, char** argv) {
-  /* initialising the ROS node creating node handle
-  for regestring it to the master and then private node handle to
-  handle the parameters */
-  ros::init(argc, argv, "spot_micro_motion_cmd"); 
-  ros::NodeHandle nh;
-  ros::NodeHandle pnh("~"); 
- 
-  SpotMicroMotionCmd node(nh,pnh); // Creating the object
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("spot_micro_motion_cmd");
 
-  ros::Rate rate(1.0/node.getNodeConfig().dt); // Defing the looping rate
+  smmc::SpotMicroMotionCmd app(node.get());
 
+  // Timer für periodisches Publizieren
+  const double hz = node->get_parameter("publish_rate_hz").get_type() != rclcpp::ParameterType::PARAMETER_NOT_SET
+                      ? node->get_parameter("publish_rate_hz").as_double()
+                      : 50.0;
+  auto period = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(1.0 / hz));
 
-  // Only proceed if servo configuration publishing succeeds
-   if (node.publishServoConfiguration()) {
-  
-    bool debug_mode = node.getNodeConfig().debug_mode;  
-    ros::Time begin;
-    /* Looking for any interupt else it will continue looping */
-    // Main loop runs indefinitely unless there is an interupt call
-    while (ros::ok())
-    {   
-        if (debug_mode) {
-          begin = ros::Time::now();
-        }
+  auto timer = node->create_wall_timer(period, [&app]() {
+    app.tick();
+  });
 
-        node.runOnce();
-        ros::spinOnce();
-        rate.sleep();
-
-        if (debug_mode) {
-          std::cout << (ros::Time::now() - begin) << std::endl;
-        }
-    }
-
-  }
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
